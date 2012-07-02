579e84ad0bffc980b8957cff65f661c1e48311ca
David Paschich <dpassage@flophouse.com>
2012-07-02 10:40:34 -0700

    Assignment 1 Walkthrough page 19: Sample storyboard view, linked to the Ruby app delegate code. I've set the identity field for the UIViewController, so I can reference it from the Rub code.

--- Calculator/app/app_delegate.rb
+++ Calculator/app/app_delegate.rb
@@ -1,5 +1,12 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    true
+    storyboard = UIStoryboard.storyboardWithName("Storyboard", bundle: NSBundle.mainBundle)
+    view_controller = storyboard.instantiateViewControllerWithIdentifier("Main")
+
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.applicationFrame)
+    @window.rootViewController = view_controller
+    @window.makeKeyAndVisible
+
+    true  
   end
 end
