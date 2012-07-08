f2a937548ba701fb259ab06dccbef1e39b3d2ff5
David Paschich <dpassage@flophouse.com>
2012-07-07 23:42:35 -0700

    Empty out the Calculator-objc project so it really is just a wrapper for the Interface Builder objects

--- Calculator-objc/Calculator-objc/CalculatorViewController.m
+++ Calculator-objc/Calculator-objc/CalculatorViewController.m
@@ -7,54 +7,32 @@
 //
 
 #import "CalculatorViewController.h"
-#import "CalculatorBrain.h"
 
 @interface CalculatorViewController ()
-@property (nonatomic) BOOL userIsInTheMiddleOfEnteringANumber;
-@property (nonatomic, strong) CalculatorBrain *brain;
 @end
 
 @implementation CalculatorViewController
 @synthesize ticker;
 @synthesize display;
-@synthesize userIsInTheMiddleOfEnteringANumber;
-@synthesize brain = _brain;
-
-- (CalculatorBrain *)brain
-{
-    if (!_brain) _brain = [[CalculatorBrain alloc] init];
-    return _brain;
-}
 
 - (IBAction)digitPressed:(UIButton *)sender {
-    NSString *digit = [sender currentTitle];
-    if (self.userIsInTheMiddleOfEnteringANumber) {
-        self.display.text = [self.display.text stringByAppendingString:digit];
-    } else {
-        self.display.text = digit;
-        self.userIsInTheMiddleOfEnteringANumber = YES;
-    }
 }
 
 - (IBAction)plusMinusPressed {
 }
+
 - (IBAction)decimalPressed {
 }
+
 - (IBAction)enterPressed {
-    [self.brain pushOperand:[self.display.text doubleValue]];
-    self.userIsInTheMiddleOfEnteringANumber = NO;
 }
+
 - (IBAction)backspacePressed {
 }
 
 - (IBAction)operationPressed:(UIButton *)sender {
-    if (self.userIsInTheMiddleOfEnteringANumber) {
-        [self enterPressed];
-    }
-    NSString *operation = [sender currentTitle];
-    double result = [self.brain performOperation:operation];
-    self.display.text = [NSString stringWithFormat:@"%g", result];
 }
+
 - (IBAction)clearPressed {
 }
 
