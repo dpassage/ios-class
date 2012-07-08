015484316efe0e6ea41a00199ffee9bc5ecdc9da
David Paschich <dpassage@flophouse.com>
2012-07-08 00:36:06 -0700

    Update CalculatorBrain to reflect sample from class for assignment 2

--- Calculator/app/CalculatorBrain.rb
+++ Calculator/app/CalculatorBrain.rb
@@ -1,52 +1,68 @@
 class CalculatorBrain
 
-  def operand_stack
-    @operand_stack = [] unless @operand_stack
-    @operand_stack
+  def program
+    self.program_stack.clone
   end
 
   def pushOperand(operand)
-    self.operand_stack.push(operand)
-  end
-  
-  def popOperand
-    if self.operand_stack.size == 0
-      0
-    else
-      self.operand_stack.pop
-    end
+    self.program_stack.push(operand)
   end
 
   def performOperation(operation)
+    self.program_stack.push(operation)
+    CalculatorBrain::run_program(self.program)
+  end
+
+  def reset_state
+    @program_stack = []
+  end
+
+  def self.description_of_program(program)
+    "Implement this in Homework #2"
+  end
+
+  def self.run_program(program)
+    stack = program.clone
+    CalculatorBrain::pop_operand_off_program_stack(stack)
+  end
+
+  def program_stack
+    @program_stack = [] unless @program_stack
+    @program_stack
+  end
+
+  def self.pop_operand_off_program_stack(stack)
     result = 0.0
-    
-    if operation == "+"
-      result = self.popOperand + self.popOperand
-    elsif operation == "*"
-      result = self.popOperand * self.popOperand
-    elsif operation == "-"
-      subtrahend = self.popOperand
-      result = self.popOperand = subtrahend
-    elsif operation == "/"
-      divisor = self.popOperand
-      result = self.popOperand / divisor unless divisor == 0
-    elsif operation == "sin"
-      result = Math::sin(self.popOperand)
-    elsif operation == "cos"
-      result = Math::cos(self.popOperand)
-    elsif operation == "sqrt"
-      result = Math::sqrt(self.popOperand)
-    elsif operation == "π"
-      result = Math::PI
-    elsif operation == "+/-"
-      result = - self.popOperand
+
+    top_of_stack = stack.pop
+    if (top_of_stack.is_a? Numeric) 
+      result = top_of_stack
+    elsif (top_of_stack.is_a? String)
+      operation = top_of_stack
+      if operation == "+"
+        result = pop_operand_off_program_stack(stack) + pop_operand_off_program_stack(stack)
+      elsif operation == "*"
+        result = pop_operand_off_program_stack(stack) * pop_operand_off_program_stack(stack)
+      elsif operation == "-"
+        subtrahend = pop_operand_off_program_stack(stack)
+        result = pop_operand_off_program_stack(stack) - subtrahend
+      elsif operation == "/"
+        divisor = pop_operand_off_program_stack(stack)
+        result = pop_operand_off_program_stack(stack) / divisor unless divisor == 0
+      elsif operation == "sin"
+        result = Math::sin(pop_operand_off_program_stack(stack))
+      elsif operation == "cos"
+        result = Math::cos(pop_operand_off_program_stack(stack))
+      elsif operation == "sqrt"
+        result = Math::sqrt(pop_operand_off_program_stack(stack))
+      elsif operation == "π"
+        result = Math::PI
+      elsif operation == "+/-"
+        result = - pop_operand_off_program_stack(stack)
+      end
     end
 
-    self.pushOperand result 
     result
   end
-  
-  def reset_state
-    @operand_stack = []
-  end
+
 end
