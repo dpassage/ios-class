ef769ff31db9000bcae68161335a69fbe00d242d
David Paschich <dpassage@flophouse.com>
2012-07-17 12:21:22 -0700

    Draw the graph with a default origin and scale. This gets us through Assignment 3 Tasks 1-6, except for the iPad mentioned in task 5.

--- Calculator/app/GraphView.rb
+++ Calculator/app/GraphView.rb
@@ -1,9 +1,32 @@
 class GraphView < UIView
+  attr_accessor :data_source
+  SCALE = 10.0
   def drawRect(rect)
-    point = CGPoint.new
-    point.x = bounds.origin.x + (bounds.size.width / 2)
-    point.y = bounds.origin.y + (bounds.size.height / 2)
-    # drawAxesInRect:originAtPoint:scale:
-    AxesDrawer.drawAxesInRect(rect, originAtPoint:point, scale:10.0)
+    middle = CGPoint.new
+    middle.x = bounds.origin.x + (bounds.size.width / 2)
+    middle.y = bounds.origin.y + (bounds.size.height / 2)
+    # drawAxesInRect:originAtmiddle:scale:
+    AxesDrawer.drawAxesInRect(bounds, originAtPoint:middle, scale:SCALE)
+
+    context = UIGraphicsGetCurrentContext()
+
+    CGContextBeginPath(context)
+
+    x_coord = bounds.origin.x
+    x_value = (x_coord - middle.x) / SCALE
+    y_value = data_source.y_value_for_x(x_value)
+    y_coord = middle.y - (SCALE * y_value)
+    CGContextMoveToPoint(context, x_coord, y_coord)
+
+    x_coord += 1
+    while x_coord < bounds.origin.x + bounds.size.width
+      x_value = (x_coord - middle.x) / SCALE
+      y_value = data_source.y_value_for_x(x_value)
+      y_coord = middle.y - (SCALE * y_value)
+      CGContextAddLineToPoint(context, x_coord, y_coord)
+      x_coord += 1
+    end
+    CGContextStrokePath(context)
+
   end
 end
--- Calculator/app/GraphViewController.rb
+++ Calculator/app/GraphViewController.rb
@@ -3,8 +3,17 @@ class GraphViewController < UIViewController
   attr_accessor :graphLabel
   attr_accessor :program
 
+  def setGraph(graph)
+    @graph = graph
+    @graph.data_source = self
+  end
+
   def viewDidLoad
     NSLog("GraphViewController loaded!")
     graphLabel.text = CalculatorBrain.descriptionOfProgram(program)
+  end
+
+  def y_value_for_x(x)
+    CalculatorBrain.runProgram(program, usingVariableValues:{ "x" => x })
   end
 end
