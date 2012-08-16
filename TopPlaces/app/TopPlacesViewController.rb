ffa0b01f354bbf026b6370d583bdd90a0286238f
David Paschich <dpassage@flophouse.com>
2012-08-15 20:40:18 -0700

    TopPlacesViewController now a dummy UITableViewController

--- TopPlaces/app/TopPlacesViewController.rb
+++ TopPlaces/app/TopPlacesViewController.rb
@@ -1,8 +1,30 @@
-class TopPlacesViewController < UIViewController
-  def loadView
-    self.view = UILabel.alloc.init
+class TopPlacesViewController < UITableViewController
+  def init
+    @cell_titles = ["foo", "bar", "baz"]
+    @cell_subtitles = ["ack", "pfft!", "whee"]
+    initWithStyle UITableViewStylePlain
+
   end
-  def viewDidLoad
-    self.view.text = "Top Places"
+
+  # UITableViewDataSource protocol methods
+
+  def numberOfSectionsInTableView(view)
+    1
+  end
+
+  def tableView(view, numberOfRowsInSection:section)
+    @cell_titles.size
+  end
+
+  def tableView(view, cellForRowAtIndexPath:indexPath)
+    NSLog("In tableView:cellForRowAtIndexPath %@", indexPath)
+    cell = self.tableView.dequeueReusableCellWithIdentifier("Top Place Cell")
+    if (!cell) 
+      cell = UITableViewCell.alloc.initWithStyle(UITableViewCellStyleSubtitle,
+                                 reuseIdentifier:"Top Place Cell")
+    end    
+    cell.textLabel.text = @cell_titles[indexPath.row]
+    cell.detailTextLabel.text = @cell_subtitles[indexPath.row]
+    cell
   end
 end
--- TopPlaces/app/app_delegate.rb
+++ TopPlaces/app/app_delegate.rb
@@ -2,10 +2,16 @@ class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     return true if RUBYMOTION_ENV == 'test'
     tab_controller = UITabBarController.alloc.init
-    view_controller = TopPlacesViewController.alloc.init
-    other_view_controller = TopPhotosViewController.alloc.init
+    top_places_vc = TopPlacesViewController.alloc.init
+    top_places_vc.title = "Top Places"
+    # top_places_vc.tabBarItem = UITabBarItem.alloc.initWithTabBarSystemItem(UITabBarSystemItemFeatured, tag:0)
 
-    tab_controller.viewControllers = [view_controller, other_view_controller]
+    top_photos_vc = TopPhotosViewController.alloc.init
+    top_photos_vc.title = "Top Photos"
+    # top_photos_vc.tabBarItem = UITabBarItem.alloc.initWithTabBarSystemItem(UITabBarSystemItemMostRecent, tag:1)
+
+    tab_controller.viewControllers = [top_places_vc, top_photos_vc]
+    tab_controller.selectedViewController = top_places_vc
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.rootViewController = tab_controller
     @window.makeKeyAndVisible
