1f902718ea8a78f869a733a32efbf299544527a6
David Paschich <dpassage@flophouse.com>
2012-08-15 22:39:21 -0700

    Now TopPlacesViewController shows a list of top places from the Flickr API. Next is setting up the UINavigationController so I can have segues.

--- TopPlaces/app/TopPlacesViewController.rb
+++ TopPlaces/app/TopPlacesViewController.rb
@@ -1,19 +1,22 @@
 class TopPlacesViewController < UITableViewController
-  def init
-    @cell_titles = ["foo", "bar", "baz"]
-    @cell_subtitles = ["ack", "pfft!", "whee"]
-    initWithStyle UITableViewStylePlain
 
+  def top_places
+    unless @top_places
+      place_array = FlickrFetcher.topPlaces
+      @top_places = place_array.sort { |a,b| a["_content"] <=> b["_content"] }
+    end
+    @top_places
+  end
+
+  def init
+     initWithStyle UITableViewStylePlain
   end
 
   # UITableViewDataSource protocol methods
 
-  def numberOfSectionsInTableView(view)
-    1
-  end
-
   def tableView(view, numberOfRowsInSection:section)
-    @cell_titles.size
+    NSLog("top_places.size is %d", self.top_places.size)
+    self.top_places.size
   end
 
   def tableView(view, cellForRowAtIndexPath:indexPath)
@@ -23,8 +26,9 @@ class TopPlacesViewController < UITableViewController
       cell = UITableViewCell.alloc.initWithStyle(UITableViewCellStyleSubtitle,
                                  reuseIdentifier:"Top Place Cell")
     end    
-    cell.textLabel.text = @cell_titles[indexPath.row]
-    cell.detailTextLabel.text = @cell_subtitles[indexPath.row]
+    place = self.top_places[indexPath.row]["_content"].split(", ", 2)
+    cell.textLabel.text = place[0]
+    cell.detailTextLabel.text = place[1]
     cell
   end
 end
