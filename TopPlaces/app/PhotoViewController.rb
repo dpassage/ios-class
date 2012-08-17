4093d6e66c8a0c196b5b139e5f7a9d5c7c7a79d1
David Paschich <dpassage@flophouse.com>
2012-08-17 13:37:34 -0700

    PVC now shows the image from the photo in a scroll view

--- TopPlaces/app/PhotoViewController.rb
+++ TopPlaces/app/PhotoViewController.rb
@@ -6,28 +6,40 @@ class PhotoViewController < UIViewController
     @photo = newphoto
   end
 
+  def shouldAutorotateToInterfaceOrientation(orientation)
+    true
+  end
   # override viewWillAppear to get the dimensions of the view for sizing the image
   # be sure to call super first
   #
   def viewWillAppear(animated)
     super
+    @image_view = UIImageView.alloc.initWithImage(self.photo.image)
+    self.view.contentSize = @image_view.bounds.size
+    self.view.minimumZoomScale=0.5
+    self.view.maximumZoomScale=6.0
+    self.view.addSubview(@image_view)
+    self.view.delegate = self
 
-    iv = UIImageView.alloc.initWithImage(self.photo.image)
-    iv.frame = self.view.bounds
-    self.view.addSubview(iv)
+    self.title = self.photo.title
   end
   # override loadview to set self.view to a scrollview
 
   def loadView
     app_frame = UIScreen.mainScreen.applicationFrame
-    content_view = UIView.alloc.initWithFrame(app_frame)
-    content_view.backgroundColor = UIColor.blackColor
-    content_view.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin |
+    scroll_view = UIScrollView.alloc.initWithFrame(app_frame)
+    scroll_view.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin |
                                     UIViewAutoresizingFlexibleWidth |
                                     UIViewAutoresizingFlexibleRightMargin |
                                     UIViewAutoresizingFlexibleTopMargin |
                                     UIViewAutoresizingFlexibleHeight |
                                     UIViewAutoresizingFlexibleBottomMargin
-    self.view = content_view
+    scroll_view.contentSize = CGSizeMake(320, 758)
+    self.view = scroll_view
+  end
+
+  # UIScrollViewDelegate protocol
+  def viewForZoomingInScrollView(scrollView)
+    return @image_view
   end
 end
