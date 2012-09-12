8f386114a65dbbf582773b10fdb8c447d2205021
David Paschich <dpassage@flophouse.com>
2012-09-12 15:49:13 -0700

    Remove tests which are no longer relevant

--- TopPlaces/spec/TopPhotosViewController_spec.rb
+++
@@ -1,8 +0,0 @@
-describe "The Top Photos view controller" do
-  tests TopPhotosViewController
-  it "has a single ui label" do
-    labels = views(UILabel)
-    labels.length.should == 1
-    labels[0].text.should == "Top Photos"
-  end
-end
--- TopPlaces/spec/TopPlacesViewController_spec.rb
+++
@@ -1,8 +0,0 @@
-describe "The Top Places view controller" do
-  tests TopPlacesViewController
-  it "has a single ui label" do
-    labels = views(UILabel)
-    labels.length.should == 1
-    labels[0].text.should == "Top Places"
-  end
-end
